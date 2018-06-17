8afd242c70418f7ea621613ac05c51fbc8c8d880
karan28598 <mr.karanagrawal@gmail.com>
2018-06-17 15:18:48 +0530

    Add Payments

---
+++ src/components/Checkout.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="col-md-6">
+  <div class="list-group">
+        <button class="list-group-item item" @click="checkout()">
+            <strong>Make Payment</strong>
+        </button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    checkout() {
+      // this.$checkout.close() 
+      // is also available.
+      this.$checkout.open({
+        name: 'Shut up and take my money!',
+        currency: 'USD',
+        amount: 99999,
+        token: (token) => {
+          // Send the token to your server
+          // for payment or subscription handling,
+          // or do whatever you want with it
+          // I don't really care. 
+        } 
+      });
+    }
+  }
+}
+</script>
