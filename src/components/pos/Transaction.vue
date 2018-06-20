c5a592318bc0c04f3ce3ff1a51934a83e1fa8ee5
karan28598 <mr.karanagrawal@gmail.com>
2018-06-20 23:56:40 +0530

    file restructure

--- src/components/pos/Transaction.vue
+++ src/components/pos/Transaction.vue
@@ -16,29 +16,12 @@
                         <span v-if="!item.editing" @click="toggleEdit(item)">{{ item.numberOfItems }}</span>
                         <input v-if="item.editing" @blur="toggleEdit(item)" type="number" v-model="item.numberOfItems">
                     </td>
-                    <td>{{ item.numberOfItems * item.item.rate }}</td>
+                    <td style="text-align:right">{{ item.numberOfItems * item.item.rate }}</td>
                     <td><a class="fa fa-times" @click="removeItem(item)">X</a></td>
                 </tr>
             </tbody>
         </table>
         <p v-if="!items.length">No items have been added.</p>
-
-        <table class="table">
-            <tbody>
-                <tr>
-                    <td>Subtotal:</td>
-                    <td>{{ subtotal }}</td>
-                </tr>
-                <tr>
-                    <td>Tax:</td>
-                    <td>{{ tax }}</td>
-                </tr>
-                <tr>
-                    <td>Total:</td>
-                    <td>{{ total }}</td>
-                </tr>
-            </tbody>
-        </table>
     </div>
 </template>
 
@@ -46,23 +29,6 @@
 
 export default {
     props: ['items', 'edit', 'remove'],
-    computed: {
-        subtotal: function() {
-            var subtotal = 0;
-
-            this.items.forEach(function(item) {
-                subtotal += item.item.rate * item.numberOfItems;
-            });
-
-            return subtotal;
-        },
-        tax: function() {
-            return this.subtotal * 0.065;
-        },
-        total: function() {
-            return this.subtotal + this.tax;
-        }
-    },
     methods: {
         toggleEdit: function(item) {
             this.edit(item);
